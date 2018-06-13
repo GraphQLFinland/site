1d45e7c189beeaa009bd9c6eb42675d99f97d3e3
Juho Vepsalainen <bebraw@gmail.com>
2018-06-13 13:15:30 +0200

    fix(index): Add page content

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,26 @@ import {
   connect,
 } from "components";
 
-const Index = ({ page = {}, conference: { speakers = [] } = {} }) => (
+const Index = ({ conference: { speakers = [] } = {} }) => (
   <>
     <section className="intro intro_home">
       <div className="intro--main">
-        <Markdown source={page.intro} />
+        <Markdown
+          source={`
+GraphQL Finland is a community-organized GraphQL conference. The first of its kind in Finland, the event consists of a workshop day and a day of talks around the topic. GraphQL Finland is organized by the same team that brought you [React Finland](https://react-finland.fi).
+
+There are still few GraphQL conferences in the world. However, GraphQL already has an amazing, vibrant community. We wanted to provide a new platform for people to share their GraphQL experience and a new place for people to learn about GraphQL.
+
+In this single track event you will learn more about GraphQL and surrounding topics while meeting some of the leading talents of the community. In addition to enjoying the event, this is your chance to explore Finland. It's still relatively warm in October, trust us.
+        `}
+        />
       </div>
       <aside className="intro--aside">
-        <Markdown source={page.secondary} />
+        <Markdown
+          source={
+            "We proudly follow [Berlin code of conduct](http://berlincodeofconduct.org/)."
+          }
+        />
       </aside>
     </section>
     <AnchorHeader level={2}>Speakers</AnchorHeader>
@@ -42,10 +54,6 @@ export default connect(`
         }
         image
       }
-    }
-    page(conferenceId: $conferenceId, id: "index") {
-      intro
-      secondary
     }
   }
 `)(Index);
